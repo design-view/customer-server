73c54c9df0fe1e3d712ea41c23ae240da98bda29
pink <pink@naver.com>
2022-01-20 17:38:48 +0900

    heroku deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port =  process.env.PORT || 8080;
 const fs = require('fs');
 const dataj = fs.readFileSync("./database.json");
 const parseData = JSON.parse(dataj);
